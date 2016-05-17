6a53a7c3d4d6078cd6e98e4644db24af681af03f
Hesham Massoud <hesham.massoud@tum.de>
2016-05-17 02:10:22 +0200

    ROUT-24: Implement search input no number validation

--- src/main/java/com/routeme/dto/SearchRequestDTO.java
+++ src/main/java/com/routeme/dto/SearchRequestDTO.java
@@ -1,11 +1,15 @@
 package com.routeme.dto;
 
+import javax.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class SearchRequestDTO {
+    @Pattern(regexp = "^[^0-9]*$", message = "Numbers are not allowed")
     @NotBlank
     private String startPoint;
 
+    @Pattern(regexp = "^[^0-9]*$", message = "Numbers are not allowed")
     @NotBlank
     private String endPoint;
 
