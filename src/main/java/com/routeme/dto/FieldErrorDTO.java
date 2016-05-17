11fa78e9f352a96dd09f124098e538fcfd329ef7
Hesham Massoud <hesham.massoud@tum.de>
2016-05-17 02:10:00 +0200

    ROUT-24: Implement error handler

---
+++ src/main/java/com/routeme/dto/FieldErrorDTO.java
@@ -0,0 +1,20 @@
+package com.routeme.dto;
+
+public class FieldErrorDTO {
+    private String field;
+    private String message;
+
+    public FieldErrorDTO(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
