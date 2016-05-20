505b52af45b68b54cb8f64987873b6f9c1d46910
Hesham Massoud <hesham.massoud@tum.de>
2016-05-20 09:09:41 +0200

    Implement PIO recommendation query

--- src/main/java/com/routeme/AppConfig.java
+++ src/main/java/com/routeme/AppConfig.java
@@ -1,12 +1,16 @@
 package com.routeme;
 
+import io.prediction.EngineClient;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppConfig {
+    public static EngineClient engineClient;
 
     public static void main(String[] args) {
+        engineClient = new EngineClient("http://localhost:8000");
         SpringApplication.run(AppConfig.class, args);
     }
 }
--- src/main/java/com/routeme/predictionio/PredictionIOClient.java
+++ src/main/java/com/routeme/predictionio/PredictionIOClient.java
@@ -3,12 +3,19 @@ package com.routeme.predictionio;
 import io.prediction.EventClient;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.gson.JsonObject;
+import com.routeme.AppConfig;
 import com.routeme.dto.RouteDTO;
 import com.routeme.model.DummyRoute;
 
@@ -16,8 +23,8 @@ public class PredictionIOClient {
     private final String accessKey = "eJErhxNseKZCZ8ijxy0fLjtj8jeHvb2ngMLoRIpwPGr5inugcbGHGSAgvJM1ZqLs";
     public static final String PREFERENCE_ROUTE_TYPE_LEASTTIME = "leastTime";
     public static final String PREFERENCE_ROUTE_TYPE_LEASTCHANGES = "leastChanges";
-    public static final String PREFERENCE_ROUTE_MODE_BUS = "bus";
-    public static final String PREFERENCE_ROUTE_MODE_UBAHN = "ubahn";
+    public static final String PREFERENCE_ROUTE_MODE_BUS = "Bus";
+    public static final String PREFERENCE_ROUTE_MODE_UBAHN = "U-bahn";
     private EventClient eventClient;
     Random rand = new Random();
     Map<String, Object> emptyProperty = ImmutableMap.of();
@@ -38,41 +45,41 @@ public class PredictionIOClient {
         this.eventClient = eventClient;
     }
 
-    public void takeRoute(String username, String routeDescription) {
-        triggerEvent(username, "take", routeDescription);
+    public void takeRoute(String userEmail, String routeDescription) {
+        triggerEvent(userEmail, "take", routeDescription);
     }
 
-    public void preferRouteType(String userId, String routeType) {
-        triggerEvent(userId, "route-preference", routeType);
+    public void preferRouteType(String userEmail, String routeType) {
+        triggerEvent(userEmail, "route-preference", routeType);
     }
 
-    public void preferRouteMode(String userId, String routeMode) {
-        triggerEvent(userId, "mode-preference", routeMode);
+    public void preferRouteMode(String userEmail, String routeMode) {
+        triggerEvent(userEmail, "mode-preference", routeMode);
     }
 
-    public void viewRoute(String userId, String routeId) {
-        triggerEvent(userId, "view", routeId);
+    public void viewRoute(String userEmail, String routeId) {
+        triggerEvent(userEmail, "view", routeId);
     }
 
-    public void viewRouteLast(String userId, String routeId) {
-        triggerEvent(userId, "view-last", routeId);
+    public void viewRouteLast(String userEmail, String routeId) {
+        triggerEvent(userEmail, "view-last", routeId);
     }
 
-    public void triggerEvent(String userId, String eventName, String targetValue) {
-        System.out.println("User " + userId + eventName + " " + targetValue);
+    public void triggerEvent(String userEmail, String eventName, String targetValue) {
+        System.out.println("User " + userEmail + eventName + " " + targetValue);
         try {
-            eventClient.userActionItem(eventName, userId, targetValue, emptyProperty);
+            eventClient.userActionItem(eventName, userEmail, targetValue, emptyProperty);
         } catch (ExecutionException | InterruptedException | IOException e) {
             System.out.println("Failed to trigger event: " + eventName + " to client because of " + e.getMessage());
         }
     }
 
-    public void addUserToClient(String userId) {
-        System.out.println("Add user " + userId);
+    public void addUserToClient(String userEmail) {
+        System.out.println("Add user " + userEmail);
         try {
-            eventClient.setUser(userId, emptyProperty);
+            eventClient.setUser(userEmail, emptyProperty);
         } catch (ExecutionException | InterruptedException | IOException e) {
-            System.out.println("Failed to add user " + userId + " to client because of " + e.getMessage());
+            System.out.println("Failed to add user " + userEmail + " to client because of " + e.getMessage());
         }
     }
 
@@ -86,8 +93,37 @@ public class PredictionIOClient {
         }
     }
 
-    public void recommendRoutes(List<RouteDTO> routes) {
-        // TODO: Query PIO with current user id and list of routes!
+    public void recommendRoutes(String userEmail, List<RouteDTO> routes) {
+        List<String> searchResultsRouteIds = new ArrayList<String>();
+        for (RouteDTO route : routes) {
+            searchResultsRouteIds.add(route.getPredictionIoId());
+        }
+        Map<String, Object> fieldsArray = new HashMap<String, Object>();
+        fieldsArray.put("name", "routeId");
+        fieldsArray.put("values", searchResultsRouteIds);
+        fieldsArray.put("bias", -1);
+        try {
+            JsonObject response = AppConfig.engineClient.sendQuery(ImmutableMap.<String, Object> of("user", userEmail,
+                    "fields", fieldsArray));
+            System.out.println(response.getAsString());
+        } catch (ExecutionException | InterruptedException | IOException e) {
+            System.out.println("Failed to query for routes to client because of " + e.getMessage());
+        }
+    }
+
+    public void addRoutesToClient(List<RouteDTO> allResults) {
+        for (int i = 0; i < allResults.size(); i++) {
+            RouteDTO routeDTO = allResults.get(i);
+            System.out.println("Add route " + routeDTO.getPredictionIoId() + " to event store");
+            try {
+                // TODO: Fix this empty string param
+                eventClient.setItem(routeDTO.getPredictionIoId(), routeDTO.getRoutePIOProperties(""));
+                triggerEvent("trainer", "take", routeDTO.getPredictionIoId());
+            } catch (ExecutionException | InterruptedException | IOException e) {
+                System.out.println("Failed to add route " + routeDTO.getPredictionIoId() + " to client because of "
+                        + e.getMessage());
+            }
+        }
     }
 
     /**
