f6db6715a7bc2d0094a4d6abeb0ae46eb2a9173f
Hesham Massoud <hesham.massoud@tum.de>
2016-05-29 19:56:15 +0200

    Release version 0.0.9

--- src/main/java/com/routeme/AppConfig.java
+++ src/main/java/com/routeme/AppConfig.java
@@ -3,9 +3,13 @@ package com.routeme;
 import io.prediction.EngineClient;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class AppConfig {
     public static EngineClient engineClient;
 
